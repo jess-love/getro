5e0b0dadf5e7c7e4a2883af13962ebe2298ac02a
Jess <91991012+jess-love@users.noreply.github.com>
2023-10-31 20:33:56 +0100

    ll

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,18 +24,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        $products = Product::all();
-      return view('index',compact('produits'));
-    }
-
-    public function produitDetail(Request $request ){
-
-        $produit = Product::find($request->id);
-        return view('product-details',compact('produit'));
-}
-
+   
 
 
 
