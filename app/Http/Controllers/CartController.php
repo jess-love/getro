b01cb2f0c1362107e1d1108af78722006f662149
Jess <91991012+jess-love@users.noreply.github.com>
2023-11-01 20:11:04 -0400

    ef

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Gloudemans\Shoppingcart\Cart as ShoppingcartCart;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 
 
@@ -10,8 +12,22 @@ class CartController extends Controller
     //
 
     public function add(){
-        
+        Cart ::add([
+            ['id' => '293ad', 
+            'name' => 'Product 1', 
+            'qty' => 1, 
+            'price' => 10.00],
+            ['id' => '4832k', 'name' => 'Product 2', 'qty' => 1, 'price' => 10.00, 'options' => ['size' => 'large']]
+          ]);
+
+          return redirect(Route('shop_cart'));
+
+    }
 
 
+    public function panier(){
+        $panier = Cart::content();
+
+        dd($panier);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,17 @@ Route::get('/login', function () {
     return view('auth.login');
 });
 
-Route::get('/product-details/{id}', [App\Http\Controllers\HomeController::class, 'produitDetail'])->name('voir_detail');
+
 Route::get('/product-list-details', [App\Http\Controllers\ProductListDefaultController::class, 'index'])->name('product-list-details');
 
 
 Route::get('/product-details/{id}', [App\Http\Controllers\HomeController::class, 'product'])->name('view_product');
 
 Route::get('/products-category', [App\Http\Controllers\HomeController::class, 'ViewCategory'])->name('CategoryProduct');
+
+Route::get('/genio','CarteController@add')->name('add_cart'); 
+
+Route::get('/gg','CarteController@panier')->name('shop_cart');
 
 Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
 
