fc5dab6ac76a50e11a0c8ee537b11d6d5467420c
Jess <91991012+jess-love@users.noreply.github.com>
2023-11-07 20:45:19 +0100

    dee

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
         return view('product-details',compact('produit'));
 
     }
-
+                                                      
     //public function ViewCategory(){
 
       // $cat = Category::all();
