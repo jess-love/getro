e26e1ebfaa1ed7e3c94b01c1c12fcc2e365e926d
Jess <91991012+jess-love@users.noreply.github.com>
2023-11-14 19:17:49 +0100

    hh

--- database/migrations/2023_11_03_030020_create_products_table.php
+++ database/migrations/2023_11_03_030020_create_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('sub_category_id');
-            $table->foreign('sub_category_id')->references('id')->on('sub_category');
             $table->string('title');
             $table->string('tag');
             $table->string('description');
