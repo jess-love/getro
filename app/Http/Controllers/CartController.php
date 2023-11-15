5ea25de8928b8a1a0043d763c2d3dd9a02e1dd1f
Jess <91991012+jess-love@users.noreply.github.com>
2023-11-14 21:39:30 -0500

    rrr

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,31 +9,13 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function addProductToCart($id){
-    $produit = Product::query()->findOrFail($id);
-    $cart = session()->get('cart',[]);
+    public function addProductToCart(){
 
-    if(isset($cart[$id])){
-        $cart[$id]['quantite']++;
-    }else{
-        $cart[$id] = [
-            "title" =>$produit->title,
-            "quantite" =>1,
-            "Prix" => $produit->unit_price,
-            "image" => $produit->main_pic
-        ];
-    }
-    session()->put('cart',$cart);
-
-    return redirect()->back()->with('success','produit a ete bien ajoute au panier');
-
+    return view('');
 }
-
 
     public function panier(){
 
-        //$panier = Cart::content();
 
-        //dd($panier);
     }
 }
