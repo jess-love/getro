acbacc9acd526c5426d36751366b618aa3b2d94e
Jess <91991012+jess-love@users.noreply.github.com>
2023-11-27 19:21:15 -0500

    jhk

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -483,7 +483,7 @@
                 </div>
                 <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
                     <button type="button" class="btn w-sm btn-light" data-bs-dismiss="modal">Close</button>
-                    <a href="{{route('remove.item')}}" class="btn w-sm btn-danger" id="remove-product">Yes, Delete It!</a>
+                    <a href="" class="btn w-sm btn-danger" id="remove-product">Yes, Delete It!</a>
                 </div>
             </div>
 
