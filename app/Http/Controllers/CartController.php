071ca6c37a9f5a651491b335c06b6478790e4d54
Jess <91991012+jess-love@users.noreply.github.com>
2023-11-29 18:49:45 +0100

    khjhj

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -25,11 +25,6 @@ class CartController extends Controller
                 'size' =>"XL",
                 'quantity' =>2,
             ];
-            Cart::create([
-                'user_id' =>Auth::user()->id,
-                'product_id' =>$product->id,
-                'qty' =>$cart['quantity']
-            ]);
         }
         session()->put('cart',$cart);
         return redirect()->back()->with('success','Product add to cart succssfully!');
