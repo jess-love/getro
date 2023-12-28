4867de4093040f6eba4230b5c29862c23a86d46d
Jess <91991012+jess-love@users.noreply.github.com>
2023-12-28 14:33:55 -0500

    Commit 28 Dec prime

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -63,23 +63,6 @@ class CartController extends Controller
 
     private function addToUserCart($product_id, $user_id, $product_qty)
     {
-//        if (!Session::has('cart')) {
-//            Session::put('cart', []);
-//        }
-//
-//        $cart = Session::get('cart', []);
-//
-//        $productKey = array_search($product_id, array_column($cart, 'product_id'));
-//        if ($productKey !== false) {
-//
-//            $cart[$productKey]['quantity'] += $product_qty;
-//
-//        } else {
-//            $cart[] = [
-//                'product_id' => $product_id,
-//                'quantity' => $product_qty,
-//            ];
-//        }
 
         $cartItem = new Cart();
         $cartItem->product_id = $product_id;
@@ -87,19 +70,6 @@ class CartController extends Controller
         $cartItem->quantity = $product_qty;
         $cartItem->save();
 
-
-//        $cartItem = new Cart();
-//        $cartItem->product_id = $product_id;
-//        $cartItem->user_id = $user_id;
-//        $cartItem->quantity = $product_qty;
-//        $cartItem->save();
-//
-//        $cart = Session::get('cart', []);
-//        $cart[] = $cartItem;
-//        session(['cart' => $cart]);
-//
-//        echo "After adding to cart: ";
-//        dd($cart);
     }
 
 
